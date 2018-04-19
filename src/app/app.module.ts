eedbb2e3156d95cc40682ac2e12a0140b820413f
Aseel Al Dallal <aseelaldallal@gmail.com>
2018-04-19 16:42:28 -0400

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,12 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-
+import { UserComponent } from './user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule
